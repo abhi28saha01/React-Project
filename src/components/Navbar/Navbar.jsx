19c9a22324bd9383d06710b20025832fcd778dd8
Abhishek Saha <145136816+abhi28saha01@users.noreply.github.com>
2024-02-27 22:13:41 +0530

    first commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import './Navbar.css'
+import search_icon_light from '../../assets/search-w.png'
+import search_icon_dark from '../../assets/search-b.png'
+import toggle_icon_dark from '../../assets/night.png'
+import toggle_icon_light from '../../assets/day.png'
+
+const Navbar = ({theme,setTheme}) => {
+
+    const toggle_mod = () => {
+        theme == "light" ? setTheme("dark") : setTheme("light");
+    }
+
+  return (
+    <div  className='navbar'>
+
+        <div className='search-box'>
+            <input type='text' placeholder='Search'/>
+            <img src={theme == "light" ? search_icon_light : search_icon_dark} alt=''/>
+        </div>
+
+        <ul>
+            <li>Categories</li>
+            <li>Website Builders</li>
+            <li>Today's deals</li>
+        </ul>
+
+        <img onClick={() => {toggle_mod()}} 
+            src={theme == "light" ? toggle_icon_dark : toggle_icon_light} 
+            className='toggle-icon'/>
+
+    </div>
+  )
+}
+
+export default Navbar;
---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
+
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/abhi28saha01/React-Project.git
+// git push -u origin main
